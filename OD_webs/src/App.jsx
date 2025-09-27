59e890a5eaaf5721e8b0ebeabec64cb668e878a1
Andres Garcia <luis.andres.garcia.lopez@uabc.edu.mx>
2025-09-27 11:37:59 -0600

    first commit

---
+++ OD_webs/src/App.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react'
+import {doc, getDoc} from 'firebase/firestore'
+import { db } from '/home/andylml/Desktop/otros_dias/OD_webs/src/firebase-config.js'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const docRef = doc(db, "users", "yoDcY5Yrf1rcVDcb4K9Y")
+
+  const getData = async () => { 
+    const docSnap = await getDoc(docRef);
+
+    console.log(docSnap.data());
+  };
+
+  useEffect( () => {
+    getData()
+
+  }, [])
+
+  
+
+  
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1 className='text-blue-500'>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
